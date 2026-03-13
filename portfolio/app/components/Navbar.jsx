c9354af008256912177987ea4e2ba6b769a80907
Uthpalawanna <uthpalawanna30@gmail.com>
2026-03-13 21:47:00 +0530

    Commit

---
+++ portfolio/app/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+    return<nav className='w-full fixed top-0 left-0 px-5 lg:px-8 xl:px-[8%] py-4 flex item-center justify-center z-50'>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-[#1f1f1f] shadow-sm'> </ul>
+    </nav>
+};
+
+export default Navbar
