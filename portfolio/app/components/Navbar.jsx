c4bda381b1d47bc2aa9e9ad9b93ce0301c58d26d
Uthpalawanna <uthpalawanna30@gmail.com>
2026-03-14 09:32:46 +0530

    Commit

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -1,9 +1,37 @@
+'use client'
+import { XMarkIcon } from '@heroicons/react/16/solid'
 import React from 'react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+
+const navLinks = [
+  { href: '#about', label: 'About' },
+  { href: '#projects', label: 'Projects' },
+  { href: '#skills', label: 'Skills' },
+  { href: '#contact', label: 'Contact' },
+]
 
 const Navbar = () => {
-    return<nav className='w-full fixed top-0 left-0 px-5 lg:px-8 xl:px-[8%] py-4 flex item-center justify-center z-50'>
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-[#1f1f1f] shadow-sm'> </ul>
+
+    const [navbarOpen, setNavbarOpen] = useState(false)
+
+  return (
+    <nav className='w-full fixed top-0 left-0 px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-center z-50'>
+      <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-[#1f1f1f] shadow-sm'>
+        {navLinks.map(({ href, label }) => (
+          <li key={href}>
+            <a href={href}>{label}</a>
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile Menu */}
+      <div className='ml-auto md:hidden'>
+        <button>
+            {navbarOpen ? <XMarkIcon/> : <Bars3Icon/>}
+        </button>
+      </div>
     </nav>
-};
+  )
+}
 
 export default Navbar
